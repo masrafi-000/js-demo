2ef964c74f55e2075848486f0c2e48639fefff61
masrafi-000 <smmasrafi000@gmail.com>
2024-08-26 01:32:03 +0600

    Updated

--- script.js
+++ script.js
@@ -18,3 +18,19 @@ function solveQuadratic (a, b, c){
 console.log(solveQuadratic(1, -3 ,2));
 console.log(solveQuadratic(1, 2, 1));
 console.log(solveQuadratic(1, 1, 1));
+
+// --------Loops-------
+
+
+let i = 0;
+while (i < 5) {
+  console.log(i);
+  i++;
+}
+// Output: 0 1 2 3 4
+
+const person = {name: "John", age: 30, city: "New York"};
+for (let key in person) {
+  console.log(key + ": " + person[key]);
+}
+// Output: name: John age: 30 city: New York
